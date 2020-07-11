475fae5d3c0b5c07e48ea1ae5c10ad16a6aa7d8f
Ian <russmoonbase@gmail.com>
2020-07-10 23:06:16 -0700

    Added character movement

---
+++ GMTK2020/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AnimatorParameters
+{
+   MoveVert
+}
+
+
+public class PlayerController : MonoBehaviour
+{
+   public float moveSpeed;
+   public float rotateSpeed;
+   public GameObject playerModel;
+
+   private float _horInput;
+   private float _vertInput;
+   private Vector3 _moveVector;
+   private Vector3 _moveInput;
+   private Vector3 _forward;
+   private Vector3 _right;
+   [SerializeField] private CharacterController _charController; // loaded in Inspector
+   [SerializeField] private Animator _animator; // loaded in Inspector
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      _horInput = Input.GetAxisRaw("Horizontal");
+      _vertInput = Input.GetAxisRaw("Vertical");
+      Movement();
+      Turn();
+      UpdateAnimations();
+   }
+
+   private void Movement()
+   {
+      _moveVector = Vector3.zero;
+      _forward = this.transform.forward * _vertInput;
+      _right = this.transform.right * _horInput;
+      _moveVector = _forward + _right;
+
+      if (_moveVector.magnitude > 1.0f)
+      {
+         _moveVector.Normalize();
+      }
+
+      _charController.Move(_moveVector * moveSpeed * Time.deltaTime);
+   }
+
+   private void Turn()
+   {
+      if (_moveVector != Vector3.zero)
+      {
+         Quaternion newRotateDir = Quaternion.LookRotation(new Vector3(_moveVector.x, 0f, _moveVector.z));
+         playerModel.transform.rotation = Quaternion.Lerp(playerModel.transform.rotation, newRotateDir, rotateSpeed * Time.deltaTime);
+      }
+   }
+
+   private void UpdateAnimations()
+   {
+      _moveInput = new Vector3(_vertInput, 0, _horInput);
+      _animator.SetFloat(AnimatorParameters.MoveVert.ToString(), _moveInput.magnitude);
+   }
+}

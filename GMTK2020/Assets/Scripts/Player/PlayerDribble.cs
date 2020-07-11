f5c0b9420a517ca884f505fb21205332f7c0f32a
Ian <russmoonbase@gmail.com>
2020-07-11 01:21:44 -0700

    initial dribbling added

---
+++ GMTK2020/Assets/Scripts/Player/PlayerDribble.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDribble : MonoBehaviour
+{
+   public GameObject playerModel;
+
+   // Start is called before the first frame update
+   void Start()
+   {
+
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+
+   }
+
+   private void OnCollisionEnter(Collision collision)
+   {
+      if (collision.gameObject.tag == "Ball")
+      {
+         Debug.Log("TRUE");
+         Rigidbody rb = collision.gameObject.GetComponent<Rigidbody>();
+
+         if (rb)
+         {
+            rb.AddForce((playerModel.transform.localRotation * Vector3.forward) * 15f);
+         }
+      }
+   }
+}

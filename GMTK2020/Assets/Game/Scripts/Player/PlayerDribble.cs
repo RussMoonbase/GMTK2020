37dfe99e29e5250f7b93a7f51670db997305cdac
Ian <russmoonbase@gmail.com>
2020-07-12 03:55:23 -0700

    Added Field Goal and Arrow

--- GMTK2020/Assets/Game/Scripts/Player/PlayerDribble.cs
+++ GMTK2020/Assets/Game/Scripts/Player/PlayerDribble.cs
@@ -5,7 +5,10 @@ using UnityEngine;
 public class PlayerDribble : MonoBehaviour
 {
    public static PlayerDribble instance;
+   public float translateSpeed;
+   public float kickPower;
 
+   public GameObject arrow;
    public GameObject playerModel;
    public GameObject target;
    public GameObject player;
@@ -14,6 +17,7 @@ public class PlayerDribble : MonoBehaviour
    public bool isKicking = false;
 
    private Vector3 _aimVector;
+   private Vector3 _arrowVector;
 
    private void Awake()
    {
@@ -31,17 +35,19 @@ public class PlayerDribble : MonoBehaviour
    {
       if (Input.GetButtonDown("R3"))
       {
-         Debug.Log("R3 pressed");
          _aimVector = target.transform.position;
          _aimVector.x = player.transform.position.x;
          target.transform.position = _aimVector;
       }
 
       //target.transform.Translate(new Vector3(Input.GetAxisRaw("MouseX"), Input.GetAxisRaw("MouseY"), 0f) * Time.deltaTime * 10f);
-      target.transform.Translate(new Vector3(Input.GetAxisRaw("MouseX"), Input.GetAxisRaw("MouseY"), 0f) * Time.deltaTime * 10f);
+      target.transform.Translate(new Vector3(Input.GetAxisRaw("MouseX"), Input.GetAxisRaw("MouseY"), 0f) * Time.deltaTime * translateSpeed);
       _aimVector = target.transform.position;
       _aimVector.y = Mathf.Clamp(_aimVector.y, 0.3f, 17f);
       target.transform.position = _aimVector;
+
+      _arrowVector = new Vector3(target.transform.position.x, target.transform.position.y, target.transform.position.z);
+      arrow.transform.LookAt(_arrowVector);
    }
 
    private void OnCollisionEnter(Collision collision)
@@ -57,7 +63,7 @@ public class PlayerDribble : MonoBehaviour
 
             if (rb)
             {
-               rb.AddForce(direction * 5f, ForceMode.Impulse);
+               rb.AddForce(direction * kickPower, ForceMode.Impulse);
             }
 
             if (ball)
